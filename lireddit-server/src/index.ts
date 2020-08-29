a6dce9d347a95ba41194fec0707deae01d464345
dmaisano <dmaisanooo@gmail.com>
2020-08-28 22:39:23 -0400

    Added User + Post entities and resolvers

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,9 +1,33 @@
 import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
+import mikroConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    dbName: "lireddit",
+  const orm = await MikroORM.init(mikroConfig);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: () => ({
+      em: orm.em,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on http://localhost:4000");
   });
 };
 
-main();
+main().catch((err) => {
+  console.log(err);
+});
---
+++ lireddit-server/src/mikro-orm.config.ts
@@ -0,0 +1,17 @@
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { User } from "./entities/User";
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.[ts]s$/, // regex pattern for the migration files
+  },
+  entities: [Post, User],
+  dbName: "lireddit",
+  type: "postgresql",
+  user: "postgres",
+  password: "postgres",
+  debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
