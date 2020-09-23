f249b4bd206d64e518452b120898ac0bd2c982eb
dmaisano <dmaisanooo@gmail.com>
2020-09-22 23:31:24 -0400

    Created register page

---
+++ lireddit-web/src/components/InputField.tsx
@@ -0,0 +1,20 @@
+import { FormControl, FormErrorMessage, FormLabel, Input } from "@chakra-ui/core";
+import { useField } from "formik";
+import React from "react";
+
+type InputFieldProps = React.HTMLAttributes<HTMLInputElement> & {
+  label: string;
+  name: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({ label, ...props }) => {
+  const [field, { error }] = useField(props);
+
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor="name">{label}</FormLabel>
+      <Input {...field} id={field.name} placeholder={props.placeholder} />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
