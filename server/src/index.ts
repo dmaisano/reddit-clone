3b7cb1142ca6e6558b4625eafba92356161b2cd2
Domenico Maisano <dmaisanooo@gmail.com>
2021-03-25 19:49:46 -0400

    MikroORM TypeGraphQL Crud

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,30 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
-import path from "path";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
 import microConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
+import { PostsResolver } from "./resolvers/posts";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostsResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log(`server started on http://localhost:4000/graphql`);
+  });
 };
 
 main().catch((err) => {
