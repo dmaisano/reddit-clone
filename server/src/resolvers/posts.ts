cc2ca36d379011fc50194ec021c3e8e996dd75ca
Domenico Maisano <dmaisanooo@gmail.com>
2021-04-26 01:41:09 -0400

    GraphQL Fetching Relationships

--- server/src/resolvers/posts.ts
+++ server/src/resolvers/posts.ts
@@ -48,24 +48,36 @@ export class PostsResolver {
     @Arg("cursor", () => String, { nullable: true }) cursor: string | null,
   ): Promise<PaginatedPosts> {
     const realLimit = Math.min(200, limit);
-    const reaLimitPlusOne = realLimit + 1;
-    const qb = getConnection()
-      .getRepository(Post)
-      .createQueryBuilder("p")
-      .orderBy('"createdAt"', "DESC")
-      .take(reaLimitPlusOne);
+    const realLimitPlusOne = realLimit + 1;
+
+    const queryParams: any[] = [realLimitPlusOne];
 
     if (cursor) {
-      qb.where('"createdAt" < :cursor', {
-        cursor: new Date(parseInt(cursor)),
-      });
+      queryParams.push(new Date(parseInt(cursor)));
     }
 
-    const posts = await qb.getMany();
+    const posts = await getConnection().query(
+      `
+      select p.*,
+      json_build_object(
+        'id', u.id,
+        'username', u.username,
+        'email', u.email,
+        'createdAt', u."createdAt",
+        'updatedAt', u."updatedAt"
+        ) creator
+      from post p
+      inner join public.user u on u.id = p."creatorId"
+      ${cursor ? `where p."createdAt" < $2` : ``}
+      order by p."createdAt" DESC
+      limit $1
+      `,
+      queryParams,
+    );
 
     return {
       posts: posts.slice(0, realLimit),
-      hasMore: posts.length === reaLimitPlusOne,
+      hasMore: posts.length === realLimitPlusOne,
     };
   }
 
